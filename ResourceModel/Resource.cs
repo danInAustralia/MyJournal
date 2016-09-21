dc10df579aaff30f3cb5b00d49a5ec00b121007b
d.piccoli <piccoli.dan@gmail.com>
2016-09-22 00:00:06 +0800

    initial setup of model to store personal files

---
+++ ResourceModel/Resource.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResourceModel
+{
+    public class Resource
+    {
+        public String Md5 { get; set; }
+        public String OriginalFileName { get; set; }
+        public MimeType Type;
+        public List<Tag> Tags { get; set; }
+    }
+
+
+}
---
+++ ResourceRepository/IResourceRepository.cs
@@ -0,0 +1,18 @@
+﻿using ResourceModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResourceRepository
+{
+    public interface IResourceRepository
+    {
+        int SaveFile();
+
+        int AddTagToFile(String md5, Tag tag);
+
+        int AddToAlbum(String albumName, Resource resource);
+    }
+}
