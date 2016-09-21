f65028115f0d9e0edecb1f4c5e43e0a20ece7b9f
d.piccoli <d.piccoli@MUR-IIID-NB-61>
2016-09-21 22:34:01 +0800

    Add project files.

---
+++ MyJournal/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MyJournal.Startup))]
+namespace MyJournal
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
